9c8a1ae85b474e1170af6c6a1ad406fccf755e1b
yigitalp-ozcn <yigitalpozcn@gmail.com>
2025-11-25 11:32:07 +0100

    Fix

--- components/organization-switcher.tsx
+++ components/organization-switcher.tsx
@@ -44,7 +44,7 @@ export function OrganizationSwitcher({
               className="data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground"
             >
               <div className="bg-sidebar-primary text-sidebar-primary-foreground flex aspect-square size-8 items-center justify-center rounded-lg">
-                <activeOrganization.logo className="size-4" />
+                {React.createElement(activeOrganization.logo, { className: "size-4" })}
               </div>
               <div className="grid flex-1 text-left text-sm leading-tight">
                 <span className="truncate font-medium">{activeOrganization.name}</span>
@@ -69,7 +69,7 @@ export function OrganizationSwitcher({
                 className="gap-2 p-2"
               >
                 <div className="flex size-6 items-center justify-center rounded-md border">
-                  <org.logo className="size-3.5 shrink-0" />
+                  {React.createElement(org.logo, { className: "size-3.5 shrink-0" })}
                 </div>
                 {org.name}
               </DropdownMenuItem>
